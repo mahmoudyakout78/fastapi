42b89d63f718b419c74c6310f1e6a4eedd6d04b9
mahmoudyakout78 <mahmoudyakout071@gmail.com>
2025-05-05 20:31:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import numpy as np
 from io import BytesIO
+import gdown
 import os
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-
+# إعداد النموذج
+file_id = "1vwlsHu7rYkN1pi7AUphi4-M6iciVfWaG"  # ID من Google Drive
 model_path = "down_detect_v1.keras"
+download_url = f"https://drive.google.com/uc?id={file_id}"
+
+# تحميل النموذج من Google Drive إذا لم يكن موجوداً
+if not os.path.exists(model_path):
+    print("Downloading model from Google Drive...")
+    gdown.download(download_url, model_path, quiet=False)
+
+# تحميل النموذج
 model = load_model(model_path)
 categories = ["Non-Standardexpected", "Standardnormal"]
 
 # إعداد التطبيق
 app = FastAPI()
 
+# إعداد CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,11 +36,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# معالجة الصورة
 def preprocess_image(image_data):
     img = load_img(image_data, target_size=(224, 224))
     img_array = img_to_array(img) / 255.0
     return np.expand_dims(img_array, axis=0)
 
+# نقطة التنبؤ
 @app.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
     try:
